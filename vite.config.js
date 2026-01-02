dd997f94f9862467c0a992d5390cda43017cdf73
dugann <53960950+dugann@users.noreply.github.com>
2026-01-02 15:23:14 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/indy-locator-web/', // <--- THIS FIXES THE 404 ERRORS
   server: {
     host: '127.0.0.1', 
     port: 0, 
